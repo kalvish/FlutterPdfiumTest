f09c01a1b1f6e7d2187eba9aab6247c4549dfec1
vishwan <vishwan@boardpac.co>
2020-05-03 22:38:03 +0530

    helpful links

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,25 @@ import 'package:streams_test/pdfium/pdfium.dart';
 
 import 'package:file_picker/file_picker.dart';
 
+//remote repo
+//https://github.com/kalvish/FlutterPdfiumTest/commits/master
+//useful links
+//pdfium doc https://pdfium.patagames.com/help/html/M_Patagames_Pdf_Pdfium_FPDF_GetPageSizes.htm
+//https://pdfium.googlesource.com/pdfium/+/refs/heads/master/samples/pdfium_test.cc
+//https://pdfium.googlesource.com/pdfium/+/refs/heads/master/fpdfsdk/fpdf_view_c_api_test.c
+//https://pdfium.googlesource.com/pdfium/+/master/public/fpdf_annot.h
+//Utf16 example https://github.com/dart-lang/ffi/issues/35
+//pdfium doc https://developers.foxitsoftware.com/resources/pdf-sdk/c_api_reference_pdfium/group___f_p_d_f_i_u_m.html#gaf31488e80db809dd21e4b0e94a266fe6
+//dart:ffi samples  https://github.com/dart-lang/samples/blob/master/ffi/primitives/primitives.dart
+//flutter device width height https://stackoverflow.com/questions/49553402/flutter-screen-size
+//android pdfium fork with text selection and search https://github.com/kalvish/android-support-pdfium/blob/master/library/src/main/java/org/benjinus/pdfium/PdfiumSDK.java
+//android pdfium pull request with text seleciton https://github.com/barteksc/PdfiumAndroid/pull/32/files
+//android barteksc PDFView https://github.com/barteksc/AndroidPdfViewer/blob/master/android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
+//android barteksc Pdfium Core https://github.com/barteksc/PdfiumAndroid/blob/103d5855f797af78a6f33f94cb306ef1c23b2290/src/main/java/com/shockwave/pdfium/PdfiumCore.java#L433
+//dartpad custom painter sample https://dartpad.dev/a1bde55a35d88ec7d58fcd0022926ad1
+//dartpad custom painter tutorial https://codewithandrea.com/videos/2020-01-27-flutter-custom-painting-do-not-fear-canvas/
+
+
 void main() async {
   // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
   // debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
@@ -138,13 +157,15 @@ class _PdfViewState extends State<PdfView> {
   void didUpdateWidget(PdfView oldWidget) {
     int width;
     int height;
+    int deviceWidth;
+    int deviceHeight;
     Pointer<FPDF_PAGE> page;
     Pointer<FPDF_DOCUMENT> doc;
     Pointer<FPDF_BITMAP> bitmap;
     Uint8List buf;
 
     int ppi = (MediaQuery.of(context).devicePixelRatio * 160).toInt();
-    ppi = 100;
+    // ppi = 100;
     if (widget.filePath == null) return;
 
     doc = loadDocument(widget.filePath);
@@ -155,8 +176,8 @@ class _PdfViewState extends State<PdfView> {
     height = fGetPageHeight(page).toInt();
     width = pointsToPixels(width, ppi).toInt();
     height = pointsToPixels(height, ppi).toInt();
-    width = MediaQuery.of(context).size.width.toInt();
-    height = MediaQuery.of(context).size.height.toInt();
+    deviceWidth = MediaQuery.of(context).size.width.toInt();
+    deviceHeight = MediaQuery.of(context).size.height.toInt();
     bitmap = fBitmapCreate(width, height, 1);
     fBitmapFillRect(bitmap, 0, 0, width, height, 0);
     fRenderPageBitmap(bitmap, page, 0, 0, width, height, 0, 0);
@@ -222,7 +243,7 @@ Check if the input annotation type available or not.
     Pointer<Double> rightP = allocate<Double>();
     Pointer<Double> bottomP = allocate<Double>();
     Pointer<Double> topP = allocate<Double>();
-    fTextGetCharBox(fpdf_textpage,0,leftP,rightP,bottomP,topP);
+    fTextGetCharBox(fpdf_textpage, 0, leftP, rightP, bottomP, topP);
     int errorFTextGetCharBox = fGetLastError();
     /*
     Close FPDF_TEXTPAGE after all related operations
